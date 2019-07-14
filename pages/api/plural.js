9f97c9f18c50826a1ba56ffc12a7cdc4c8491ea7
John Lindquist <johnlindquist@gmail.com>
2019-07-14 09:24:41 -0600

    hi

--- pages/api/plural.js
+++ pages/api/plural.js
@@ -3,6 +3,5 @@ import { NounInflector } from "natural"
 const nounInflector = new NounInflector()
 
 export default function handle(req, res) {
-  console.log("posting", req.body.word)
   res.end(nounInflector.pluralize(req.body.word))
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,15 @@
 import axios from "axios"
 
-// IndexPage.getInitialProps = async function() {
-//   const message = await fetch(`http://localhost:3000/api/hello`, {
-//     method: "POST",
-//     body: "apple"
-//   }).then(r => r.text())
-
-//   return { message }
-// }
-
 import { useState, useEffect, useCallback } from "react"
 
-function IndexPage(props) {
+function IndexPage() {
   const [word, setWord] = useState("")
   const [pluralWord, setPluralWord] = useState("")
 
   useEffect(() => {
     if (!word) return
-
     ;(async word => {
-      const response = await axios.post(`http://localhost:3000/api/hello`, {
+      const response = await axios.post(`http://localhost:3000/api/plural`, {
         word
       })
 
